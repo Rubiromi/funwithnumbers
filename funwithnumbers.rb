67606f2b1770868666808fb8b1befbfb62628f71
Rubiromi <hiromi.deyoung@gmail.com>
2014-05-23 14:20:16 -0600

    added customer's choice function

--- funwithnumbers.rb
+++ funwithnumbers.rb
@@ -1,7 +1,24 @@
 puts "please pick a number"
-num1 = gets.chomp.to_i
+num1 = gets.chomp.to_f
 
 puts "please pick next number"
-num2 = gets.chomp.to_i
+num2 = gets.chomp.to_f
 
-puts num1 + num2
+puts "Please choose the method between addition\(+\), subtraction\(-\), division\(/\), and multiplication\(*\)."
+method = gets.chomp
+
+while method != '+' && method != '-' && method != '/' && method != '*'
+  puts "invailed method. please pick the method again."
+  method = gets.chomp
+end
+
+case method
+  when '+'
+    puts num1 + num2
+  when '-'
+    puts num1 - num2
+  when '/'
+    puts num1 / num2
+  when '*'
+    puts num1 * num2
+end
